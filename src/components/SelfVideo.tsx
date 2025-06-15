636c8d92deee60e55658a89f70bc752472e56f9d
Yug34 <yug.gjr@gmail.com>
2025-06-15 14:27:23 +0530

    made selfVideo draggable

--- src/components/SelfVideo.tsx
+++ src/components/SelfVideo.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 import useChatStore from '@/store/core'
+import { useDraggable } from '@dnd-kit/core'
 
 type SelfVideoProps = {
   selfVideoRef: React.RefObject<HTMLVideoElement | null>
@@ -7,17 +8,27 @@ type SelfVideoProps = {
 
 const SelfVideo: React.FC<SelfVideoProps> = ({ selfVideoRef }) => {
   const { status } = useChatStore()
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({ id: 'self-video' })
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined
 
   return (
-    <video
+    <div
+      ref={setNodeRef}
+      style={style}
       className={cn({
         'absolute bottom-4 right-4 w-[400px] h-[400px] bg-black': status === 'Connected',
         'absolute bottom-0 right-0 w-full h-full bg-black -z-10 opacity-70': status !== 'Connected',
       })}
-      ref={selfVideoRef}
-      autoPlay
-      playsInline
-    />
+      {...attributes}
+      {...listeners}
+    >
+      <video className="w-full h-full" ref={selfVideoRef} autoPlay playsInline />
+    </div>
   )
 }
 
